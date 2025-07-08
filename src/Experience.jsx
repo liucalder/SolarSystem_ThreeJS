4cb1cd8f850e5f2b36a884825773a2935a87958f
liucalder <liucalder@gmail.com>
2025-07-08 11:59:22 -0600

    orbitcontrols

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,11 +1,13 @@
-import { useLoader, useThree } from '@react-three/fiber'
+import { useLoader, useThree, extend } from '@react-three/fiber'
 import * as THREE from 'three'
 import { useEffect } from 'react'
 import SolarSystem from './SolarSystem.jsx'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
+extend({ OrbitControls })
 
 export default function Experience() {
-  const { scene } = useThree()
-
+  const { camera, gl, scene } = useThree()
   const texture = useLoader(THREE.TextureLoader, '/pictures/space.jpg')
 
   useEffect(() => {
@@ -14,6 +16,7 @@ export default function Experience() {
 
   return (
     <>
+      <orbitControls args={ [ camera, gl.domElement ] } />
       <SolarSystem />
     </>
   )

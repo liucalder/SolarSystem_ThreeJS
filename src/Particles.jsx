b2e337b589f0c357a6021ea94f808f8546a9223d
liucalder <liucalder@gmail.com>
2025-07-13 13:22:41 -0600

    coordinates

--- src/Particles.jsx
+++ src/Particles.jsx
@@ -1,22 +1,31 @@
 import * as THREE from 'three'
 
 export default function Particles({ color }) {
-  const particlesCount = 100000
-  const spread = 3000 
+  const particlesCount = 25000
+  const innerRadius = 450
 
-  // new particles array
   const positions = new Float32Array(particlesCount * 3)
+
   for (let i = 0; i < particlesCount; i++) {
-    positions[i * 3 + 0] = (Math.random() - 0.5) * spread  // X position
-    positions[i * 3 + 1] = (Math.random() - 0.5) * spread  // Y position
-    positions[i * 3 + 2] = (Math.random() - 0.5) * spread  // Z position
+    // Make random spherical coordinates
+    const radius = innerRadius + Math.random() * 1500 // radius r
+    const theta = Math.acos(2 * Math.random() - 1) // Polar angle (θ)
+    const phi = 2 * Math.PI * Math.random() // azimuthal angle (φ)
+
+    // spherical coordinates converted into cartesian coodinates
+    // Formulas: x = ρsin(φ)cos(θ), y = ρsin(φ)sin(θ), and z = ρcos(φ).
+    const x = radius * Math.sin(theta) * Math.cos(phi)
+    const y = radius * Math.sin(theta) * Math.sin(phi)
+    const z = radius * Math.cos(theta)
+
+    positions[i * 3 + 0] = x
+    positions[i * 3 + 1] = y
+    positions[i * 3 + 2] = z
   }
-  
-  // create the geometry
+
   const geometry = new THREE.BufferGeometry()
   geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
 
-  // Create material
   const material = new THREE.PointsMaterial({
     color: color,
     size: 2,
